b66f32ff40842182cf9e6729978b8ee7fd03605b
tanguobing <970672980@qq.com>
2020-06-19 17:55:04 +0800

    修改index.vue和APP

--- App.vue
+++ App.vue
@@ -2,6 +2,32 @@
 export default {
 	onLaunch: function() {
 		console.log('App Launch');
+		// #ifdef APP-PLUS
+		// 检测升级
+		uni.request({
+		    url: 'https://uniapp.dcloud.io/update', //检查更新的服务器地址
+		    data: {
+		        appid: plus.runtime.appid,
+		        version: plus.runtime.version,
+		        imei: plus.device.imei
+		    },
+		    success: (res) => {
+		        if (res.statusCode == 200 && res.data.isUpdate) {
+		            let openUrl = plus.os.name === 'iOS' ? res.data.iOS : res.data.Android;
+		            // 提醒用户更新
+		            uni.showModal({
+		                title: '更新提示',
+		                content: res.data.note ? res.data.note : '是否选择更新',
+		                success: (showResult) => {
+		                    if (showResult.confirm) {
+		                        plus.runtime.openURL(openUrl);
+		                    }
+		                }
+		            })
+		        }
+		    }
+		})
+		// #endif
 	},
 	onShow: function() {
 		console.log('App Show');
@@ -13,11 +39,24 @@ export default {
 </script>
 
 <style>
-/* 解决头条小程序组件内引入字体不生效的问题 */
-/* #ifdef MP-TOUTIAO */
-@font-face {
-	font-family: uniicons;
-	src: url('/static/uni.ttf');
-}
-/* #endif */
+	/* 解决头条小程序组件内引入字体不生效的问题 */
+	/* #ifndef APP-PLUS-NVUE */
+	/*uni.css -通用组件、模板样式库*/
+	@import './common/uni.css';
+	
+	
+	.pages-index-index{
+		background-color: #E97352;
+		
+	}
+	
+	/* #endif */
+	/* #ifdef MP-360 */
+	::-webkit-scrollbar {width: 4px; height: 4px; background-color: transparent;}
+	::-webkit-scrollbar-track {background-color: transparent;}
+	::-webkit-scrollbar-thumb {background-color: #ddd;}
+	::-webkit-scrollbar-thumb:hover {background-color: #ccc;}
+	::-webkit-scrollbar-thumb:active {background-color: #bbb;}
+	::-webkit-scrollbar-corner {background: #ffffff;}
+	/* #endif*/
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,58 @@
 <template>
 	<view class="container">
-		<text :class="className" :style="classDemo">详见：</text>
-		<text v-if="showBoolean">{{number}}</text>
-		<image class="logo" src="/static/timg.png"></image>
-		<button type="default" :class="buttonType" @click="handleClick">{{showName}}</button>
-		<uni-link :href="href" :text="href"></uni-link>
+		<image class="" src="/static/one-piece2.png"></image>
+		
+		<!-- <text :class="className" :style="classDemo">详见：</text> -->
+		
+		<!-- <text v-if="showBoolean">{{number}}</text> -->
+		
+		<!-- <object data="/static/title.svg" type="image/svg+xml"></object> -->
+		
+		<view class="uni-margin-wrap">
+			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item>
+					<view class="swiper-item">
+						<image class="logo" src="/static/timg.png"></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item ">B</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item ">C</view>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		
+		<view class="swiper-list">
+			<view class="uni-list-cell uni-list-cell-pd">
+				<view class="uni-list-cell-db">指示点</view>
+				<switch :checked="indicatorDots" @change="changeIndicatorDots" />
+			</view>
+			<view class="uni-list-cell uni-list-cell-pd">
+				<view class="uni-list-cell-db">自动播放</view>
+				<switch :checked="autoplay" @change="changeAutoplay" />
+			</view>
+		</view>
+		
+		<view class="uni-padding-wrap">
+			<view class="uni-common-mt">
+				<text>幻灯片切换时长(ms)</text>
+				<text class="info">{{duration}}</text>
+			</view>
+			<slider @change="durationChange" :value="duration" min="500" max="2000" />
+			<view class="uni-common-mt">
+				<text>自动播放间隔时长(ms)</text>
+				<text class="info">{{interval}}</text>
+			</view>
+			<slider @change="intervalChange" :value="interval" min="2000" max="10000" />
+		</view>
+		
+		<!-- <button type="default" :class="buttonType" @click="handleClick">{{showName}}</button> -->
+		
+		<!--<uni-link :href="href" :text="href"></uni-link>-->
+		
 	</view>
 </template>
 
@@ -12,6 +60,11 @@
 	export default {
 		data() {
 			return {
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500,
 				className:'classNames',
 				showBoolean:'true',
 				showName:"隐藏",
@@ -25,26 +78,51 @@
 			handleClick() {
 				this.showBoolean = !this.showBoolean;
 				this.showName = this.showBoolean ? "显示":"隐藏";
+			},
+			changeIndicatorDots(e) {
+					this.indicatorDots = !this.indicatorDots
+				},
+				changeAutoplay(e) {
+					this.autoplay = !this.autoplay
+				},
+				intervalChange(e) {
+					this.interval = e.detail.value
+				},
+				durationChange(e) {
+					this.duration = e.detail.value
+				}
 			}
-		}
 	}
 </script>
 
 <style>
-	.pages-index-index{
-		background-color: #E97352;
-	}
 	.container {
-		padding: 20px;
+		/* padding: 20px; */
 		font-size: 14px;
 		line-height: 24px;
 	}
+	/* swiper */
+	.uni-margin-wrap {
+		width:100%;
+		background-color: #FFFFFF;
+		/* margin:0 30rpx; */
+	}
+	.swiper {
+		height: 600rpx;
+	}
+	.swiper-item {
+		display: block;
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
+	}
+	/* swiper */
 	.logo{
 		box-shadow: 0px 31px 38px 2px rgba(100,100,100,0.38);
 	}
 	.buttonName{
 		font-weight: bold;
-		color: #ffffff;
+		color: #FFFFFF;
 		background: radial-gradient(circle, #F9CB12,#F9D213, #F85803);
 		box-shadow: 0px 2px 10px 4px rgba(100,100,100,0.5);
 	}
